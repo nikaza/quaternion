a18fe64e36b97c5da8d610c775fb8a27998d2b94
nikaza <nikoskazazakis@gmail.com>
2016-06-21 15:33:35 +0100

    Add files via upload

---
+++ build_script.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e # Exit if there is an error
+
+scriptname=$0
+fqname=`readlink -f $scriptname`
+QuaternionTop=`dirname $fqname`
+
+# Print root directory
+echo -- QuaternionTop $QuaternionTop
+
+if [[ `expr match "${QuaternionTop}" '\(.*src$\)'` ]]; then
+	QuaternionBuild=$QuaternionTop/build
+	QuaternionInstall=$QuaternionTop/install
+else
+	QuaternionBuild=$QuaternionTop/build
+	QuaternionInstall=$QuaternionTop/install
+fi
+echo -- Build dir is $QuaternionBuild
+echo -- Install dir is $QuaternionInstall
+
+# Process input arguments
+while getopts "j:t:r" flag
+do
+	case ${flag} in
+	  j) CPUS=${OPTARG};;
+	  t) toolset=${OPTARG};;
+	  r) resetBuild=true;;
+	  *) echo>&2 "invalid option ${flag}i"; usage; exit 1;;
+	esac
+done
+
+# Delete all previous build and install folders
+if [[ "$resetBuild" == "true" ]]; then
+	rm -rf build install
+fi
+
+CMAKE=cmake
+CPACK=cpack
+
+# Use default compiler versions
+case $toolset in
+	llvm) 
+	  export CC=clang
+	  export CXX=clang++
+	;;
+	gcc)
+	  export CC=gcc
+	  export CXX=g++
+	;;
+	*)
+	  export CC=gcc
+	  export CXX=g++
+	;;
+esac
+
+BUILDDIR=${QuaternionBuild}
+INSTALLDIR=${QuaternionInstall}
+
+QuaternionSrc=${TOP}/src
+
+mkdir -p ${BUILDDIR}
+pushd ${BUILDDIR}
+
+${SBCMAKE} ${CMAKE} \
+	${QuaternionTop}
+
+${CMAKE} --build .
+${CMAKE} --build . --target install
+
+popd
